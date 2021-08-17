ea910823e5b89dd74c2bc6061fa6acb986410a77
Takanori Fujiki <42742231+TKNRFJK1208@users.noreply.github.com>
2021-08-17 00:59:41 -0700

    First Commit

---
+++ www/js/itemdata.js
@@ -0,0 +1,33 @@
+// resort list data
+var itemSet = [
+    {
+        id: 0,
+        name: 'Aloha Dress', 
+        active: true,
+        price: '$79.00',
+        keyImage:'item-01.jpeg',
+        otherImage: [
+            'item-02.jpeg',
+            'item-03.jpeg',
+            'item-04.jpeg',
+            'item-05.jpeg',
+            'item-06.jpeg',
+            'item-07.jpeg',
+            'item-08.jpeg',
+            'item-09.jpeg',
+            'item-10.jpeg'
+        ],
+        shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        size: 'XS/S/M'
+    },
+    {id: 1, name: 'Aloha Dress', active: true,price: '$89.00', keyImage:'item-02.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 2, name: 'Beta Dress', active: true,price: '$99.00', keyImage:'item-03.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 3, name: 'Gamma Dress', active: true,price: '$59.00', keyImage:'item-04.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 4, name: 'Delta Dress', active: true,price: '$59.00', keyImage:'item-05.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 5, name: 'Epsilon Dress', active: true,price: '$79.00', keyImage:'item-06.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 6, name: 'Zeta Dress', active: true,price: '$69.00', keyImage:'item-07.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 7, name: 'Eta Dress', active: true,price: '$79.00', keyImage:'item-08.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 8, name: 'Theta Dress', active: true,price: '$99.00', keyImage:'item-09.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: 9, name: 'Iota Dress', active: true,price: '$99.00', keyImage:'item-10.jpeg', shortDesc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+];
+
